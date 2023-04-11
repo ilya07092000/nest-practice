cac091500e106cdae2caa90738206d71948c5a24
ilya07092000 <ischenko2012@gmail.com>
2023-04-11 18:31:32 +0300

    feat: user service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './users.model';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectModel(User) private userRespository: typeof User) {}
+
+  async createUser(dto: CreateUserDto) {
+    const user = await this.userRespository.create(dto);
+    return user;
+  }
+
+  async getAllUsers() {
+    const users = await this.userRespository.findAll();
+    return users;
+  }
+}
