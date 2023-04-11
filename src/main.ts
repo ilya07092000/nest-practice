550df5bdde2bae8143fcc8aa0dfe99a49ccc871a
ilya07092000 <ischenko2012@gmail.com>
2023-04-11 17:26:15 +0300

    fix: port env case

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function start() {
-  const PORT = process.env.port || 8080;
+  const PORT = process.env.PORT || 8080;
   const app = await NestFactory.create(AppModule);
 
   await app.listen(PORT, () => console.log(`Server Started On Port ${PORT}`));
