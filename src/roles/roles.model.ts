25d53e8b0614c4ffadcccbc20b7ad50d6ab26383
ilya07092000 <ischenko2012@gmail.com>
2023-04-12 17:49:15 +0300

    feat: roles model

---
+++ src/roles/roles.model.ts
@@ -0,0 +1,45 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  BelongsToMany,
+  Column,
+  DataType,
+  Model,
+  Table,
+} from 'sequelize-typescript';
+import { User } from 'src/users/users.model';
+import { UserRoles } from './user-roles.model';
+
+interface RoleCreationAttrs {
+  value: string;
+  description: string;
+}
+
+@Table({ tableName: 'roles' })
+export class Role extends Model<Role, RoleCreationAttrs> {
+  @ApiProperty({ example: '1' })
+  @Column({
+    type: DataType.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    unique: true,
+  })
+  id: number;
+
+  @ApiProperty({ example: 'ADMIN' })
+  @Column({
+    type: DataType.STRING,
+    unique: true,
+    allowNull: false,
+  })
+  value: string;
+
+  @ApiProperty({ example: 'administrator' })
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  description: string;
+
+  @BelongsToMany(() => User, () => UserRoles)
+  users: User[];
+}
