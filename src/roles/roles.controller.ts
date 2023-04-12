0639986e92e2a017ccd33f0d214904883d83dc15
ilya07092000 <ischenko2012@gmail.com>
2023-04-12 17:49:07 +0300

    feat: roles controller

--- src/roles/roles.controller.ts
+++ src/roles/roles.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { CreateRoleDto } from './dto/create-role.dto';
+import { Role } from './roles.model';
+import { RolesService } from './roles.service';
 
+@ApiTags('Roles Endpoint')
 @Controller('roles')
-export class RolesController {}
+export class RolesController {
+  constructor(private rolesService: RolesService) {}
+
+  @ApiResponse({ status: 200, type: Role })
+  @ApiOperation({ summary: 'Creates Role' })
+  @Post()
+  async create(@Body() rolesDto: CreateRoleDto) {
+    return this.rolesService.createRole(rolesDto);
+  }
+
+  @ApiResponse({ status: 200, type: Role })
+  @ApiOperation({ summary: 'Get Role By Value' })
+  @Get('/:value')
+  async getByValue(@Param('value') value: string) {
+    return this.rolesService.getRoleByValue(value);
+  }
+}
