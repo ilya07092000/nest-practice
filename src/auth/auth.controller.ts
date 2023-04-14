9d865d0c648b2413e719d280ab0895d141f24025
ilya07092000 <ischenko2012@gmail.com>
2023-04-14 16:22:25 +0300

    added basic routes to auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('/registration')
+  async registration(@Body() userDto: CreateUserDto) {
+    const result = await this.authService.registration(userDto);
+
+    return result;
+  }
+
+  @Post('/login')
+  async login(@Body() userDto: CreateUserDto) {
+    const result = await this.authService.login(userDto);
+
+    return result;
+  }
+}
