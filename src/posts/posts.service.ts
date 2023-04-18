8c97e2d19d56cbea7e270fa8dee1f60e65495248
ilya07092000 <ischenko2012@gmail.com>
2023-04-18 17:51:52 +0300

    feat: post service

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  HttpException,
+  HttpStatus,
+  Injectable,
+} from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { User } from 'src/users/users.model';
+import { CreatePostDto } from './dto/create-post.dto';
+import { Post } from './posts.model';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  constructor(
+    @InjectModel(Post) private postRepository: typeof Post,
+    @InjectModel(User) private userRepository: typeof User,
+  ) {}
+
+  async createPost(dto: CreatePostDto) {
+    const user = this.userRepository.findByPk(dto.userId);
+
+    if (!user) {
+      throw new BadRequestException({ message: 'User Does Not Exist' });
+    }
+
+    const post = await this.postRepository.create(dto);
+    return post;
+  }
+
+  async getPosts() {
+    return await this.postRepository.findAll();
+  }
+
+  async getPostById(id: string) {
+    const post = await this.postRepository.findByPk(id);
+
+    if (!post) {
+      throw new HttpException('Post Not Found', HttpStatus.NOT_FOUND);
+    }
+
+    return post;
+  }
+
+  async detePostById(id: string) {
+    const post = await this.getPostById(id);
+    return post.destroy();
+  }
+}
