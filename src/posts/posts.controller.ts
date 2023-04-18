a77b43dc897507a0facfeecdae42735bad6e221a
ilya07092000 <ischenko2012@gmail.com>
2023-04-18 17:51:44 +0300

    feat: posts contolller

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,49 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { Roles } from 'src/auth/roles-auth.decorator';
+import { RolesGuard } from 'src/auth/roles.guard';
+import { CreatePostDto } from './dto/create-post.dto';
+import { PostsService } from './posts.service';
 
+@ApiTags('Posts')
 @Controller('posts')
-export class PostsController {}
+export class PostsController {
+  constructor(private postService: PostsService) {}
+
+  @Post()
+  @UseGuards(JwtAuthGuard)
+  async create(@Request() req, @Body() dto: Omit<CreatePostDto, 'userId'>) {
+    const post: CreatePostDto = { ...dto, userId: req.user.id };
+
+    return this.postService.createPost(post);
+  }
+
+  @Get()
+  @UseGuards(JwtAuthGuard)
+  async getPosts() {
+    return this.postService.getPosts();
+  }
+
+  @Get(':id')
+  @UseGuards(JwtAuthGuard)
+  async getById(@Param('id') id: string) {
+    return this.postService.getPostById(id);
+  }
+
+  @Delete(':id')
+  @UseGuards(RolesGuard)
+  @Roles('admin')
+  async deleteById(@Param('id') id: string) {
+    return this.postService.detePostById(id);
+  }
+}
