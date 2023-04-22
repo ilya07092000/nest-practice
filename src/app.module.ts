10490818b626b150446e1e53dd02e814c1e712d6
ilya07092000 <ischenko2012@gmail.com>
2023-04-22 15:09:25 +0300

    generate files service, controler, module

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,7 @@ import { UserRoles } from './roles/user-roles.model';
 import { AuthModule } from './auth/auth.module';
 import { PostsModule } from './posts/posts.module';
 import { Post } from './posts/posts.model';
+import { FilesModule } from './files/files.module';
 
 @Module({
   imports: [
@@ -32,6 +33,7 @@ import { Post } from './posts/posts.model';
     RolesModule,
     AuthModule,
     PostsModule,
+    FilesModule,
   ],
   controllers: [],
   providers: [],
