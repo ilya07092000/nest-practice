0911d8171c66cbd84042d2e7290187430b7f69a9
ilya07092000 <ischenko2012@gmail.com>
2023-04-22 15:35:19 +0300

    feat: files service

--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,4 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as uuid from 'uuid';
 
 @Injectable()
-export class FilesService {}
+export class FilesService {
+  async createFile(file): Promise<string> {
+    if (!file) return '';
+
+    try {
+      const fileName = uuid.v4() + '.jpg';
+      const filePath = path.resolve(__dirname, '..', 'static');
+
+      if (!fs.existsSync(filePath)) {
+        fs.mkdirSync(filePath, { recursive: true });
+      }
+      fs.writeFileSync(path.join(filePath, fileName), file.buffer);
+      return fileName;
+    } catch (e) {
+      console.error(e);
+      throw new HttpException(
+        'Write File error',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+}
